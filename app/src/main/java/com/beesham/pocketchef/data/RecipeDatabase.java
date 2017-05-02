282ef37eea43ddd5299c841aae3d119c986a9bdc
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-05-01 20:08:54 -0400

    Implement database class

--- app/src/main/java/com/beesham/pocketchef/data/RecipeDatabase.java
+++ app/src/main/java/com/beesham/pocketchef/data/RecipeDatabase.java
@@ -1,8 +1,16 @@
 package com.beesham.pocketchef.data;
 
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
 /**
  * Created by Beesham on 5/1/2017.
  */
 
+@Database(version=RecipeDatabase.VERSION)
 public class RecipeDatabase {
+    public static final int VERSION = 1;
+
+    @Table(Columns.SavedRecipesColumn.class) public static final String SAVED_RECIPES = "savedRecipes";
+    @Table(Columns.SearchedRecipesColumn.class) public static final String SEARCHED_RECIPES = "searchedRecipes";
 }
